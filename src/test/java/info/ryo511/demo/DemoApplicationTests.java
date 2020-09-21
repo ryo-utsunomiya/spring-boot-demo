611650c894ed4b29a73a02bb7b12da3fe53f10c9
Ryo Utsunomiya <millionsnowliving@gmail.com>
2020-09-21 18:05:51 +0900

    Created by Spring Initializer

---
+++ src/test/java/info/ryo511/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package info.ryo511.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
